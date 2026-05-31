2d91e1c95a0b997f7c6ab9c2d43aaba7f5316a39
Alpha_Digital_market|ADM <web.digital2030maroc@gmail.com>
2026-05-31 21:13:47 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Store loaded");
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if (menuBtn && mobileMenu) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
+  });
+}
+
+// Close mobile menu when clicking a link
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("open");
+  });
+});
+
+// Newsletter form demo
+const newsletterForm = document.querySelector(".newsletter-form");
+if (newsletterForm) {
+  newsletterForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thanks for subscribing!");
+    newsletterForm.reset();
+  });
+}

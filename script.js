79f662cbc449e980fe41ba5af758cb3ea9539a28
Alpha_Digital_market|ADM <web.digital2030maroc@gmail.com>
2026-06-05 20:49:00 +0200

    update

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const menuBtn = document.getElementById("menuBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-
-if (menuBtn && mobileMenu) {
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("open");
-  });
-}
-
-// Close mobile menu when clicking a link
-document.querySelectorAll(".mobile-menu a").forEach(link => {
-  link.addEventListener("click", () => {
-    mobileMenu.classList.remove("open");
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Bienvenue sur INLAYA PARIS !");
 });
-
-// Newsletter form demo
-const newsletterForm = document.querySelector(".newsletter-form");
-if (newsletterForm) {
-  newsletterForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Thanks for subscribing!");
-    newsletterForm.reset();
-  });
-}
